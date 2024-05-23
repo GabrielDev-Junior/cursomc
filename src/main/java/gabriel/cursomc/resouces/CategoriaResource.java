805e2621e83d04290b0b6abc60e12597e0533f11
Gabriel A. Amorim <gabrielavelinoamorim@gmail.com>
2024-05-23 14:43:38 -0300

    Testando aprimeira classe de dominio - Catedoria

--- src/main/java/gabriel/cursomc/resouces/CategoriaResource.java
+++ src/main/java/gabriel/cursomc/resouces/CategoriaResource.java
@@ -1,15 +1,28 @@
 package gabriel.cursomc.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import gabriel.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
